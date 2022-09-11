c0e57fd48ea9f44096512cb151fb66d78e02431c
Deshan Nawanjana <deshan.uok@gmail.com>
2022-09-11 09:56:09 +0530

    Object Class

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ Gravity.Scene = class {
         this.element = document.createElement('gravity')
         // objects array
         this.objects = []
-        // position of view
-        this.position = { x : 0, y : 0 }
-        // size of scene
-        this.size = { x : 400, y : 300 }
         // system resistance
         this.resistance = 0.01
         // system gravity
         this.gravity = 10
+        // position of view
+        this.position = { x : 0, y : 0 }
+        // size of scene
+        this.size = { x : 400, y : 300 }
         // set options
         this.set(options || {})
     }
@@ -24,18 +24,21 @@ Gravity.Scene = class {
     set(options) {
         // return if null or non object input
         if(options === null || typeof options !== 'object') { return }
-        // update position if given
-        if('position' in options) { this.position = options.position }
-        // update size if given
-        if('size' in options) { this.size = options.size }
         // update resistance if given
         if('resistance' in options) { this.resistance = options.resistance }
         // update gravity if given
         if('gravity' in options) { this.gravity = options.gravity }
-        // update element width
-        this.element.style.width = this.size.x + 'px'
-        // update element height
-        this.element.style.height = this.size.y + 'px'
+        // update position if given
+        if('position' in options) { this.position = options.position }
+        // update size if given
+        if('size' in options) {
+            // update size
+            this.size = options.size
+            // update element width
+            this.element.style.width = this.size.x + 'px'
+            // update element height
+            this.element.style.height = this.size.y + 'px'
+        }
     }
     // method to add objects
     add(object) {
@@ -54,5 +57,79 @@ Gravity.Scene = class {
     // method to render frame
     render() {
 
+    }
+}
+
+// object class
+Gravity.Object = class {
+    // constructor
+    constructor(options) {
+        // create element
+        this.element = document.createElement('obj')
+        // physically enabled flag
+        this.physical = true
+        // friction factor
+        this.friction = 0.05
+        // elasticity
+        this.elasticity = 0.2
+        // weight
+        this.weight = 50
+        // current collide objects
+        this.collide = []
+        // visibility flag
+        this.visible = true
+        // position
+        this.position = { x : 0, y : 0 }
+        // size
+        this.size = { x : 50, y : 50 }
+        // texture
+        this.texture = null
+        // set options
+        this.set(options || {})
+    }
+    // method to set options
+    set(options) {
+        // return if null or non object input
+        if(options === null || typeof options !== 'object') { return }
+        // update physical flag if given
+        if('physical' in options) { this.physical = options.physical }
+        // update friction if given
+        if('friction' in options) { this.friction = options.friction }
+        // update elasticity if given
+        if('elasticity' in options) { this.elasticity = options.elasticity }
+        // update weight if given
+        if('weight' in options) { this.weight = options.weight }
+        // update visibility flag if given
+        if('visible' in options) {
+            // update visibility flag
+            this.visible = options.visible
+            // update element visibility
+            this.element.style.display = this.visible ? 'block' : 'none'
+        }
+        // update position if given
+        if('position' in options) {
+            // update position
+            this.position = options.position
+            // update element left
+            this.element.style.left = this.position.x + 'px'
+            // update element top
+            this.element.style.top = this.position.y + 'px'
+        }
+        // update size if given
+        if('size' in options) {
+            // update size
+            this.size = options.size
+            // update element width
+            this.element.style.width = this.size.x + 'px'
+            // update element height
+            this.element.style.height = this.size.y + 'px'
+        }
+        // update texture if given
+        if('texture' in options) {
+            // texture size
+            this.texture = options.texture
+            // update element texture
+            this.element.setAttribute('texture', this.texture.id)
+        }
     }
 }
